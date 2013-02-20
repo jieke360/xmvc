a68cbdf4331434484fde8681d42eea571318690f
xiaoqulai <xiaoqulai@80683c55-4d8d-747d-1a62-a830fa663739>
2013-02-20 10:09:35 +0000

    初始导入。

---
+++ trunk/xmvc-framework-website/src/info/txtfile/app/test/PointConvert.java
@@ -0,0 +1,15 @@
+package info.txtfile.app.test;
+
+import xmvc.core.ModelConvert;
+import xmvc.core.modelconvertplug.IModelConvert;
+
+public class PointConvert  extends ModelConvert implements IModelConvert{
+	public Object convertObjectValue(Object arg0) {
+		String str = (String) arg0;
+		String[] xy = str.split(",");
+		Point p = new Point();
+		p.setX( Integer.parseInt(xy[0]));
+		p.setY( Integer.parseInt(xy[1])) ;
+		return p;
+	}
+}
---
+++ trunk/xmvc-framework-website/src/info/txtfile/app/test/TestModel.java
@@ -0,0 +1,70 @@
+package info.txtfile.app.test;
+
+import java.util.List;
+
+import xmvc.core.Processor;
+
+
+public class TestModel extends Processor {
+
+	private static final long serialVersionUID = -6050465713367923176L;
+
+	private int testInt;
+	private Integer testInteger;
+	private String testString;
+	
+	TestModel model;
+	List<TestModel> models;
+	TestModel[] testModelArr;
+	
+	
+	public List<TestModel> getModels() {
+		return models;
+	}
+
+	public void setModels(List<TestModel> models) {
+		this.models = models;
+	}
+
+	public TestModel getModel() {
+		return model;
+	}
+
+	public void setModel(TestModel model) {
+		this.model = model;
+	}
+
+	public int getTestInt() {
+		return testInt;
+	}
+
+	public void setTestInt(int testInt) {
+		this.testInt = testInt;
+	}
+
+	public Integer getTestInteger() {
+		return testInteger;
+	}
+
+	public void setTestInteger(Integer testInteger) {
+		this.testInteger = testInteger;
+	}
+
+	public String getTestString() {
+		return testString;
+	}
+
+	public void setTestString(String testString) {
+		this.testString = testString;
+	}
+
+	public TestModel[] getTestModelArr() {
+		return testModelArr;
+	}
+
+	public void setTestModelArr(TestModel[] testModelArr) {
+		this.testModelArr = testModelArr;
+	}
+	
+	
+}
